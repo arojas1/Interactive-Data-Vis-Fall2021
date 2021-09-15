abb54e76922993ac07d20a241eac4588bc18a204
Ashley Rojas <arojas1@gradcenter.cuny.edu>
2021-09-15 13:14:05 -0400

    1.3 assignment

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,33 @@
-console.log('hello world');
+const label = document.getElementById("label");
+const input = document.getElementById("name-input");
+const submit = document.getElementById("name-submit");
+
+var age = 0;
+
+const nameInfo = {
+    name: "",
+    age: "0",
+}
+
+function updateName() {
+    nameInfo.name = input.value;
+    label.innerText = "Thank you, " + nameInfo.name + ". Made a mistake? Change your name here:";
+    submit.innerText = "Change"
+    updateInfo()
+}
+
+function increaseAge() {
+    age += 1
+    nameInfo.age = age;
+    updateInfo()
+}
+
+function decreaseAge() {
+    age -= 1
+    nameInfo.age = age;
+    updateInfo()
+}
+
+function updateInfo(){
+    document.getElementById("printInfo").innerHTML= "Your Name: " + nameInfo.name + "<br/> Age:" + nameInfo.age
+}
