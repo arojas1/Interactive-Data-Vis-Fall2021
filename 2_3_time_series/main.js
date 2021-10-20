1d3e22e7334f3054586008e975710809113e51c3
Ashley Rojas <arojas1@gradcenter.cuny.edu>
2021-10-20 18:26:08 -0400

    2.3 assignment (updated)

--- 2_3_time_series/main.js
+++ 2_3_time_series/main.js
@@ -10,14 +10,14 @@ d3.csv('../data/usHeatExtremes.csv', d => {
     // country: d.Entity,
     // population: +d.Population
     state: d.State,
-    heatchange: d['Change in 95 percent Days'],
-    lat: d.Lat
+    heatchange: +d['Change in 95 percent Days'],
+    lat: +d.Lat
   }
 }).then(data => {
   // console.log(data);
 
   datafiltered = data.filter(d => d.state == "NY")
-  console.log(datafiltered);
+  // console.log(datafiltered);
 
   datafiltered.sort((a,b) => d3.ascending(a.lat, b.lat) || d3.ascending(a.heatchange, b.heatchange))
 
@@ -30,6 +30,8 @@ d3.csv('../data/usHeatExtremes.csv', d => {
     .domain(d3.extent(datafiltered, d => d.heatchange))
     .range([height-margin, margin])
 
+  console.log(yScale.domain()[0])
+
   // CREATE SVG ELEMENT
   const svg = d3.select("#container")
     .append("svg")
@@ -37,42 +39,48 @@ d3.csv('../data/usHeatExtremes.csv', d => {
     .attr("height", height)
 
   // BUILD AND CALL AXES
+  svg.append("g")
+    .attr("class", "x-axis")
+    .style("transform", `translate(0px, ${width-margin}px)`)
+    .call(d3.axisBottom(xScale))
+
+  svg.append("g")
+    .attr("class", "y-axis")
+    .style("transform", `translate(${margin}px,0px)`)
+    .call(d3.axisLeft(yScale))
 
   // LINE GENERATOR FUNCTION
-  const lineGen = d3.line()
-  .x(d => xScale(d.lat))
-  .y(d => yScale(d.heatchange))
+  // const lineGen = d3.line()
+  // .x(d => xScale(d.lat))
+  // .y(d => yScale(d.heatchange))
+
+  //area generator
+  // Plot.areaY(datafiltered, {x: "lat", y2: "heatchange"}).plot()
+  const area = d3.area()
+    .x(d => xScale(d.lat))
+    .y0(height-margin)
+    .y1(d => yScale(d.heatchange))
 
   const stategroup = d3.groups(datafiltered, d => d.state, d => d.lat).map(([key, state, lat]) => datafiltered)
   
   // console.log('stategroups :>>',stategroup)
 
   // DRAW LINE
-  svg.selectAll(".line")
-    .data(stategroup)
-    .join("path")
-    .attr("class", "line")
-    .attr("stroke", "black")
-    .attr("fill", "none")
-    .attr("d", d => lineGen(d))
+  // svg.selectAll(".line")
+  //   .data(stategroup)
+  //   .join("path")
+  //   .attr("class", "line")
+  //   .attr("stroke", "black")
+  //   .attr("fill", "none")
+  //   .attr("d", d => lineGen(d))
 
-  svg.append("g")
-    .attr("class", "x-axis")
-    .style("transform", `translate(0px, ${width-margin}px)`)
-    .call(d3.axisBottom(xScale))
-    // .selectAll("text")
-    // .style("font-size", "medium")
-
-  svg.append("g")
-    .attr("class", "y-axis")
-    .style("transform", `translate(${margin}px,0px)`)
-    .call(d3.axisLeft(yScale))
-    // .selectAll("text")
-    // .attr("y", 0)
-    // .attr("x", 5)
-    // .attr("dy", "1.5em")
-    // .attr("transform", "rotate(90)")
-    // .style("text-anchor", "middle")
-    // .style("font-size", "large")
+  // Draw area
+  svg.selectAll(".area")
+  .data(stategroup)
+  .join("path")
+  .attr("class", "area")
+  .attr("stroke", "black")
+  .attr("fill", "black")
+  .attr("d", d => area(d))
 
 });
